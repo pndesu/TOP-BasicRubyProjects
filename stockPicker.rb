53f20c4c026c379b6d41bca1f7a7e8dc032ddfe4
pndesu <tranvanphucnguyen1@gmail.com>
2022-12-20 20:36:19 +1100

    Initial commit

--- stockPicker.rb
+++ stockPicker.rb
@@ -1,17 +1,25 @@
-def stockPicker(stocks)
-    length = stocks.size
-    max = maxBuyIndex = maxSellIndex = 0
-    stocks.each_with_index do |buyStock, buyIndex|
-        stocks[buyIndex+1..].each_with_index do |sellStock, sellIndex|
-            if (sellStock - buyStock > max)
-                max = sellStock - buyStock
-                maxBuyIndex = buyIndex
-                maxSellIndex = sellIndex + buyIndex + 1
-            end
-        end
-    end
-    p [maxBuyIndex, maxSellIndex]
+# def stockPicker(stocks)
+#     length = stocks.size
+#     max = maxBuyIndex = maxSellIndex = 0
+#     stocks.each_with_index do |buyStock, buyIndex|
+#         stocks[buyIndex+1..].each_with_index do |sellStock, sellIndex|
+#             if (sellStock - buyStock > max)
+#                 max = sellStock - buyStock
+#                 maxBuyIndex = buyIndex
+#                 maxSellIndex = sellIndex + buyIndex + 1
+#             end
+#         end
+#     end
+#     p [maxBuyIndex, maxSellIndex]
+# end
+
+# stocks = [17,6,4,9,15,8,18,6,1,10]
+# stockPicker(stocks)
+input = [17,3,6,9,15,8,6,1,10]
+
+def stock_picker(prices)
+    arr = prices.each_with_index.to_a.combination(2).max_by{|buy,sell| sell[0]-buy[0]}
+  results = prices.each_with_index.to_a.combination(2).max_by{|buy,sell| sell[0]-buy[0]}.map{|price, i| i}
 end
 
-stocks = [17,6,4,9,15,8,18,6,1,10]
-stockPicker(stocks)
+puts(stock_picker(input))
